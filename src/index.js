153b01e83d49b1dbf68ef212742c749684e71d92
Anandkumar Patel <avpatel2005@gmail.com>
2023-04-11 03:03:11 -0400

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    toDo = e.target['new-task-description'].value;
+    buildToDo(toDo);
+    form.reset();
+  })
 });
+
+function buildToDo(toDo){
+  let list = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = "x";
+  list.textContent = `${toDo} `;
+  list.appendChild(btn);
+
+  let taskList = document.querySelector('#tasks');
+  taskList.appendChild(list);
+
+}
+
+function handleDelete(event){
+  event.target.parentNode.remove();
+}
